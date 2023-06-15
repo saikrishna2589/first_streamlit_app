ecc51d94740f4880a0e760f17fa47f2247187be6
saikrishna2589 <saikrishna2589@gmail.com>
2023-06-15 11:47:50 +1000

    Update streamlit_app.py
    updated header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.title('My Mom New Healthy Diner')
+streamlit.header('Breakfast Favourites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale,Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard=Boiled Free-Range Egg')
