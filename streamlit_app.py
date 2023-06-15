dc0d48c730925e0c2548c979235831bab27152a4
saikrishna2589 <saikrishna2589@gmail.com>
2023-06-15 12:47:40 +1000

    Update streamlit_app.py
    fixing punctuation kine 15multiselct line

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 #Lets put a pick list here so they can pick the fruit they want to exclude
-streamlit.multiselect('pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("pick some fruits:", list(my_fruit_list.index))
  
  #display the table on the page
 streamlit.dataframe(my_fruit_list)
